87541e540d803e39386424dc6bb8a723147374e7
Doron Brand <Guruda1@gmail.com>
2019-07-19 01:04:35 +0300

    fix refresh

--- routes/posts.js
+++ routes/posts.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     console.log("GET /posts/new To create Posts....");
     
-    res.redirect('/posts/new');
+    res.end();
 });
 
 // Private POST users/add
