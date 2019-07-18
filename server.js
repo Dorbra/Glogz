8649ce9b75e135d3b7e4b3d6c893c92e6600bf7c
Doron Brand <Guruda1@gmail.com>
2019-07-18 11:25:16 +0300

    deploy

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser'),
-    cors = require('cors'),
-    PORT = process.env.PORT || 5000,
-    config = require('config');
+  app = express(),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  path = require("path"),
+  cors = require('cors'),
+  PORT = process.env.PORT || 5000,
+  config = require('config');
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({
-    extended: true
-})); 
+  extended: true
+}));
 
 // MongoDB setup
 const db = config.get('mongoURI');
-mongoose.connect(db, 
-        {useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(db,
+  { useNewUrlParser: true, useCreateIndex: true })
   .then(() => console.log("MongoDB Connected!"))
   .catch(err => console.log(err));
 
@@ -28,11 +29,12 @@ app.use("/posts", require("./routes/posts"));
 // ========== SETUP =============== //
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+  // Set static folder
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
